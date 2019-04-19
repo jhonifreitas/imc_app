1831af2fc3acb0b7affa5cb47c3c3f3505d072e9
jonathan08 <jonathan@cosmunsoftwares.com.br>
2019-04-19 14:37:00 -0300

    developing form

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,14 +8,70 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
+  String _message;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Calculadora de IMC'),
+        backgroundColor: Colors.black12,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: () {
+
+            },
+          )
+        ],
       ),
-      body: Center(
-        child: Column(),
+      backgroundColor: Colors.white70,
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.person,
+                  size: 120,
+                  color: Colors.black12,
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text('Calcular'),
+                  textColor: Colors.white,
+                  color: Colors.black12,
+                  onPressed: () {
+
+                  },
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(this._message ?? '')
+              ],
+            )
+          ],
+        ),
       )
     );
   }
